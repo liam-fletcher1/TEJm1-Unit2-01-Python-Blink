d10b72485c7781f847d9d5c20c4e4372231c5599
Liam <90276819+liam-fletcher1@users.noreply.github.com>
2021-11-23 16:16:38 -0500

    added python code

---
+++ Blink.py
@@ -0,0 +1,14 @@
+"""Example for Pico. Turns on the built-in LED."""
+import board
+import digitalio
+import time
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+  led.value = True
+  time.sleep(0.5)
+  led.value = False
+  time.sleep(0.5)
+  
